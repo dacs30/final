74b6247e779538d0816aa64a72bdbd9e1e7d90b0
Danilo Correia <60863302+dacs30@users.noreply.github.com>
2022-02-23 14:47:08 -0500

    added bar-chart as example of chart in react

--- react-app-files/src/App.js
+++ react-app-files/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import BarChart from './charts/BarChart';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>React App</h1>
+        <BarChart />
       </header>
     </div>
   );

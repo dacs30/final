ae80a4001aa1558cd245cbf11eb0655e7269f3ec
orestropi <oropi@wpi.edu>
2022-02-26 16:37:58 -0500

    added footer

--- react-app-files/src/Components/MainFooter.js
+++ react-app-files/src/Components/MainFooter.js
@@ -5,8 +5,21 @@ import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 
 function MainFooter() {
+
+    function changeBackground(image) {
+        if (image == 1){
+   document.body.style.backgroundImage = "url('https://cdn.glitch.global/42ef66cc-8c79-4874-aa40-002089184c30/R%2BM.jpg?v=1645905015019')";}
+        if (image == 2){
+   document.body.style.backgroundImage = "url('https://cdn.glitch.global/42ef66cc-8c79-4874-aa40-002089184c30/sunglasRM.png?v=1645907068512')";}
+}
+
+
+
     return (
         <footer className='footer'>
+            <h1>Backround Change</h1>
+<button onclick={changeBackground(1)} style="font-size:48px;color:red;background-color:black;border-radius: 30%; "> <i class="fa-solid fa-heart fa-beat"></i></button>&nbsp&nbsp&nbsp&nbsp
+    <button onclick={changeBackground(2)} style="font-size:48px;color:yellow;background-color:black;border-radius: 30%; "> <i class="fa-regular fa-sun fa-fade" style="--fa-beat-fade-opacity: 0.13; --fa-beat-fade-scale: 1.675;"></i></button>
             <Box>
                 <Container maxWidth="lg">
                     <Grid alignContent={'center'} container spacing={3}>

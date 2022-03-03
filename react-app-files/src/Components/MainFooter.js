af2dd7d368390ab7113e9cfc26418d60c8de10a8
Danilo Correia <60863302+dacs30@users.noreply.github.com>
2022-03-03 12:13:43 -0500

    fix

--- react-app-files/src/Components/MainFooter.js
+++ react-app-files/src/Components/MainFooter.js
@@ -23,16 +23,16 @@ function MainFooter() {
                                     Contributors
                                 </Typography>
                                 <Typography variant="subtitle1" color="white" >
-                                    <a href="https://github.com/dacs30">Danilo Correia </a>
+                                    <a target={'_blank'} href="https://github.com/dacs30">Danilo Correia </a>
                                 </Typography>
                                 <Typography variant="subtitle1" color="white" >
-                                    <a href="https://github.com/dcjdunn2">Craig James Dunn </a>
+                                    <a target={'_blank'} href="https://github.com/cjdunn2">Craig James Dunn </a>
                                 </Typography>
                                 <Typography variant="subtitle1" color="white" >
-                                    <a href="https://github.com/orestropi">Orest Ropi</a>
+                                    <a target={'_blank'} href="https://github.com/orestropi">Orest Ropi</a>
                                 </Typography>
                                 <Typography variant="subtitle1" color="white" >
-                                    <a href="https://github.com/orestropi">Maddison Caten</a>
+                                    <a target={'_blank'} href="https://github.com/mcaten">Maddison Caten</a>
                                 </Typography>
                             </Grid>
                             <Grid item xs={12} sm={6} md={6}>
